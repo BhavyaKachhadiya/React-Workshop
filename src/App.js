74526bf643707c3db093b56bda6f1e88d30fd879
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-09-22 17:21:43 +0530

    React-Workshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Menu from './Component/Menu';
+import Footer from './Component/Footer';
+import { Route, Routes } from "react-router-dom";
+import Login from './Component/Login';
+import Home from './Component/Home';
+import React from 'react';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='container fluid'>
+        <section className='row'>
+          <Menu></Menu>
+        </section>
+       
+        <section className="row" >
+            <Routes>
+              <Route path="/" element={<Home></Home>}></Route>
+              <Route path="/login" element={<Login></Login>}></Route>
+            </Routes>
+            </section>
+
+            <section className="row">
+            <Footer></Footer>
+            </section>
+      </section>     
+        
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ src/userdata.js
@@ -0,0 +1,7 @@
+    const userdata=[
+        {name : "Vandan", email : "vandan@gmail.com", mobile : 1234567890},
+        {name : "Hard" , email : "hard@gmail.com", mobile : 1234567899},
+        {name : "Bhavy", email : "bhavy@gmail.com", mobile : 1234567889}
+    ]
+
+export default userdata
